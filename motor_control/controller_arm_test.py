aad780959c25051b20fa23e477c598b38373ee7c
hirorobebe <54560132+hirorobebe@users.noreply.github.com>
2019-10-30 07:49:28 +0000

    self文試験的に記述

--- motor_control/controller_arm_test.py
+++ motor_control/controller_arm_test.py
@@ -1,85 +1,99 @@
 #!/usr/bin/env python
-# license removed for brevity
+
 import rospy
 import math
-#from std_msgs.msg import String
 from std_msgs.msg import Float64
 
-def controller1(theta1):
-   pub = rospy.Publisher('chatter', Float64, queue_size=10)
-   rospy.init_node('controller1', anonymous=True)
-   rate = rospy.Rate(1) # 1hz
-   while not rospy.is_shutdown():
-#       theta1 = 0.5
-       rospy.loginfo(theta1)
-       pub.publish(theta1)
-       rate.sleep()
-       
-def controller2(theta2):
-   pub = rospy.Publisher('chatter', Float64, queue_size=10)
-   rospy.init_node('controller2', anonymous=True)
-   rate = rospy.Rate(1) # 1hz
-   while not rospy.is_shutdown():
-#       theta2 = 0.5
-       rospy.loginfo(theta2)
-       pub.publish(theta2)
-       rate.sleep()
+def controller_all(angle1, angle2, angle3, angle4, angle5):
+    pub1 = rospy.Publisher('tilt1_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('tilt2_controller/command', Float64, queue_size=10)
+    pub3 = rospy.Publisher('tilt3_controller/command', Float64, queue_size=10)
+    pub4 = rospy.Publisher('tilt4_controller/command', Float64, queue_size=10)
+    pub5 = rospy.Publisher('tilt5_controller/command', Float64, queue_size=10)
+    rospy.init_node('controller_all', anonymous=True)
+    rate = rospy.Rate(1) # 1hz
+    while not rospy.is_shutdown():
+        rospy.loginfo("angle1=%f",angle1)
+        rospy.loginfo("angle2=%f",angle2)
+        rospy.loginfo("angle3=%f",angle3)
+        rospy.loginfo("angle4=%f",angle4)
+        rospy.loginfo("angle5=%f",angle5)
+        pub1.publish(angle1)
+        pub2.publish(angle2)
+        pub3.publish(angle3)
+        pub4.publish(angle4)
+        pub5.publish(angle5)
+        rate.sleep()
 
-def controller3(theta3):
-   pub = rospy.Publisher('chatter', Float64, queue_size=10)
-   rospy.init_node('controller3', anonymous=True)
-   rate = rospy.Rate(1) # 1hz
-   while not rospy.is_shutdown():
-#       theta3 = 0.5
-       rospy.loginfo(theta3)
-       pub.publish(theta3)
-       rate.sleep()
+def limit(angle, LIM_ANG_P, LIM_ANG_M):
+    if angle < LIM_ANG_M:
+        angle_lim = LIM_ANG_M
+    elif angle > LIM_ANG_P:
+        angle_lim = LIM_ANG_P
+    else:
+        angle_lim = angle    
+     return angle_lim  
 
-def controller4(theta4):
-   pub = rospy.Publisher('chatter', Float64, queue_size=10)
-   rospy.init_node('controller4', anonymous=True)
-   rate = rospy.Rate(1) # 1hz
-   while not rospy.is_shutdown():
-#       theta4 = 0.5
-       rospy.loginfo(theta4)
-       pub.publish(theta4)
-       rate.sleep()
+def arm_control_top(x, y, z):
+    L_12 = 0.076
+    L_23 = 0.0825
+    L_34 = 0.094 + 0.10
+#    L_4H = 0.10
 
-def controller5(theta5):
-   pub = rospy.Publisher('chatter', Float64, queue_size=10)
-   rospy.init_node('controller4', anonymous=True)
-   rate = rospy.Rate(1) # 1hz
-   while not rospy.is_shutdown():
-#       theta5 = 0.5
-       rospy.loginfo(theta5)
-       pub.publish(theta5)
-       rate.sleep()
+    R_24 = math.sqrt(x**2 + y**2)
+    Z_24 = z 
+    goal_length = math.sqrt(x**2+y**2+z**2)
 
-def controller_all():
-   L_12 = 0.076
-   L_23 = 0.0825
-   L_34 = 0.094 + 0.10
-   x = raw_input('x: ')
-   y = raw_input('y: ')
-   z = raw_input('z: ')
-   R_24 = math.sqrt(x**2 + y**2)
-   Z_24 = z + L_12
-   theta_2d = math.acos((L_23**2)-(L_34**2)+(R_24**2)+(Z_24**2))/(2*L_23*math.sqrt((R_24**2)+(Z_24**2))))
-   theta_3d = math.acos(((L_34**2)-(L_23**2)+(R_24**2)+(Z_24**2))/(2*L_34*math.sqrt((R_24**2)+(Z_24**2))))
+#    if(goal_length > (L_23+ L_34 + L_4H)):
+    if(goal_length > (L_23+ L_34)):
+         print( "out of range" )
+
+    theta_2d = math.acos((L_23**2)-(L_34**2)+(R_24**2)+(Z_24**2))/(2*L_23*math.sqrt((R_24**2)+(Z_24**2))))
+    theta_3d = math.acos(((L_34**2)-(L_23**2)+(R_24**2)+(Z_24**2))/(2*L_34*math.sqrt((R_24**2)+(Z_24**2))))
 #   theta_4d = math.atan2(Z_24, R_24);
-   theta1 = math.atan2(x,y)
-   theta2 = 0
-   theta3 = theta_2d + theta_3d
-   theta4 = 0
-   theta5 = 0
-   controller1(theta1)
-   controller2(theta2)
-   controller3(theta3)
-   controller4(theta4)
-   controller5(theta5)
-         
+
+    angle1 = math.atan2(x,y)
+    angle2 = 0.0
+    angle3 = theta_2d + theta_3d
+    angle4 = 0.0
+    angle5 = 0.0 
+
+    LIM_ANG_P1 = 1.0
+    LIM_ANG_M1 = 0.0
+    LIM_ANG_P2 = 1.0
+    LIM_ANG_M2 = 0.0
+    LIM_ANG_P3 = 1.0
+    LIM_ANG_M3 = 0.0
+    LIM_ANG_P4 = 1.0
+    LIM_ANG_M4 = 0.0
+    LIM_ANG_P5 = 1.0
+    LIM_ANG_M5 = 0.0 
+
+    angle_lim1 = limit(angle1, LIM_ANG_P1, LIM_ANG_M1)
+    angle_lim2 = limit(angle2, LIM_ANG_P2, LIM_ANG_M2)
+    angle_lim3 = limit(angle3, LIM_ANG_P3, LIM_ANG_M3)
+    angle_lim4 = limit(angle4, LIM_ANG_P4, LIM_ANG_M4)
+    angle_lim5 = limit(angle5, LIM_ANG_P5, LIM_ANG_M5)
+
+    controller_all(angle_lim1, angle_lim2, angle_lim3, angle_lim4, angle_lim5)
+#    controller_all(angle1, angle2, angle3, angle4, angle5)
+
 if __name__ == '__main__':
-   try:
-       controller_all()
-   except rospy.ROSInterruptException:
-       pass
+    x = 2.0
+    y = 1.0
+    z = 0.0
+    try:
+        arm_control_top(x, y, z)
+    except rospy.ROSInterruptException:
+        pass
+
+####################################################################################
+
+def class RuloBot():
+    if use_arm:
+        arm = arm_control_top(x, y, z)
+
+def arm_control_top(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
